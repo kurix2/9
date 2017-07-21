ab3e18b9bb20575742643cdd65565d808bd935c0
Yazuki Wolf <okami3@gmail.com>
2017-07-21 20:08:07 +0900

    Block Shooter Gimmick now randomizes an array of shot types

--- Teleplatformer/Assets/Scripts/ShotInstantiator.cs
+++ Teleplatformer/Assets/Scripts/ShotInstantiator.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class ShotInstantiator : MonoBehaviour {
 
-    public GameObject shot;
+    public GameObject[] shot;
     public Transform glassReceiver;
     public float shotRate;
+    public bool randomRotation=false;
 
 	// Use this for initialization
 	void Start () {
@@ -23,8 +24,16 @@ public class ShotInstantiator : MonoBehaviour {
         {
             yield return new WaitForSeconds(shotRate);
             Quaternion spawnRotation = glassReceiver.rotation;
-            GameObject shotInstance = Instantiate(shot, transform.position, spawnRotation)as GameObject;
+            GameObject shotInstance = Instantiate(shot[Random.Range(0,shot.Length)], transform.position, spawnRotation)as GameObject;
             shotInstance.GetComponent<MoveToTerminate>().terminationPoint = glassReceiver;
+            if (randomRotation == true)
+            {
+                int horFlip = Random.Range(0,2);
+                int verFlip = Random.Range(0,2);
+                shotInstance.transform.Rotate(new Vector3(horFlip * 180, verFlip * 180));
+                print("hor is" + horFlip);
+                print("ver is" + verFlip);
+            }
         }
     }
 }
